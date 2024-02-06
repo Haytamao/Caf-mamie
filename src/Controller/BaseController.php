8c5b96fc2932fa5d870be6a2748e83002761869e
Your Name <you@example.com>
2024-02-06 14:38:07 +0100

    create form

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -5,6 +5,10 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\CafeType;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Cafe;
+use Doctrine\ORM\EntityManagerInterface;
 
 class BaseController extends AbstractController
 {
@@ -24,6 +28,24 @@ class BaseController extends AbstractController
     public function boisson(): Response
     {
         return $this->render('base/boisson.html.twig', [
+        ]);
+    }
+    #[Route('/ajoutCafe', name: 'app_ajoutCafe')]
+    public function ajoutCafe(Request $request, EntityManagerInterface $em): Response
+    {
+        $cafe = new Cafe();
+        $form = $this->createForm(CafeType::class, $cafe);
+        if($request->isMethod('POST')){
+            $form->handleRequest($request);
+            if ($form->isSubmitted()&&$form->isValid()){
+                $em->persist($contact);
+                $em->flush();
+                $this->addFlash('notice','Message envoyé');
+                return $this->redirectToRoute('app_ajoutCafe');
+            }
+            }
+        return $this->render('base/ajoutCafe.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
---
+++ src/Entity/Cafe.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CafeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CafeRepository::class)]
+class Cafe
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 30)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 30)]
+    private ?string $prix = null;
+
+    #[ORM\Column(length: 30)]
+    private ?string $origine = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $description = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrix(): ?string
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(string $prix): static
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getOrigine(): ?string
+    {
+        return $this->origine;
+    }
+
+    public function setOrigine(string $origine): static
+    {
+        $this->origine = $origine;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
